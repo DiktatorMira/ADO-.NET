5789dd5e620d77b1301f2666a34545704ad8c49a
Diktator <dychervony@gmail.com>
2024-02-27 20:13:37 +0200

    Изменил дз за 26.02.2024

--- Dz26.02.2024/Dz26.02.2024/Context.cs
+++ Dz26.02.2024/Dz26.02.2024/Context.cs
@@ -7,52 +7,57 @@ using System.Threading.Tasks;
 
 namespace Dz26._02._2024 {
     public class Context : DbContext{
-        public DbSet<Countries> Сountries { get; set; }
-        public DbSet<Continent> Сontinents { get; set; }
+        public DbSet<Countries> countries { get; set; }
+        public DbSet<Continent> сontinents { get; set; }
         public Context(DbContextOptions<Context> options) : base(options) {
-            Continent europe = new Continent { Title = "Европа" };
-            Continent northAmerica = new Continent { Title = "Северная Америка" };
-            Continent africa = new Continent { Title = "Африка" };
-            Continent asia = new Continent { Title = "Азия" };
-            Сontinents?.AddRange(europe, northAmerica, africa, asia);
-            SaveChanges();
-            Countries ukraine = new Countries {
-                Title = "Украина",
-                Capital = "Киев",
-                Population = 37937820,
-                Area = 603628,
-                Continent = europe
-            };
-            Countries germany = new Countries {
-                Title = "Германия",
-                Capital = "Берлин",
-                Population = 83200000,
-                Area = 357592,
-                Continent = europe
-            };
-            Countries mexico = new Countries {
-                Title = "Мексика",
-                Capital = "Мехико",
-                Population = 126700000,
-                Area = 1973000,
-                Continent = northAmerica
-            };
-            Countries egypt = new Countries {
-                Title = "Египет",
-                Capital = "Каир",
-                Population = 109300000,
-                Area = 1002000,
-                Continent = africa
-            };
-            Countries china = new Countries{
-                Title = "Китай",
-                Capital = "Пекин",
-                Population = 1412000000,
-                Area = 9597000,
-                Continent = asia
-            };
-            Сountries?.AddRange(ukraine, germany, mexico, egypt, china);
-            SaveChanges();
+            if (Database.EnsureCreated()) {
+                var cont = new List<Continent> {
+                    new Continent { Title = "Европа" },
+                    new Continent { Title = "Северная Америка" },
+                    new Continent { Title = "Африка" },
+                    new Continent { Title = "Азия" }
+                };
+                var count = new List<Countries> {
+                    new Countries {
+                        Title = "Украина",
+                        Capital = "Киев",
+                        Population = 37937820,
+                        Area = 603628,
+                        Continent = cont.FirstOrDefault(c => c.Title == "Европа")
+                    },
+                    new Countries {
+                        Title = "Германия",
+                        Capital = "Берлин",
+                        Population = 83200000,
+                        Area = 357592,
+                        Continent = cont.FirstOrDefault(c => c.Title == "Европа")
+                    },
+                    new Countries {
+                        Title = "Мексика",
+                        Capital = "Мехико",
+                        Population = 126700000,
+                        Area = 1973000,
+                        Continent = cont.FirstOrDefault(c => c.Title == "Северная Америка")
+                    },
+                    new Countries {
+                        Title = "Египет",
+                        Capital = "Каир",
+                        Population = 109300000,
+                        Area = 1002000,
+                        Continent = cont.FirstOrDefault(c => c.Title == "Африка")
+                    },
+                    new Countries{
+                        Title = "Китай",
+                        Capital = "Пекин",
+                        Population = 1412000000,
+                        Area = 9597000,
+                        Continent = cont.FirstOrDefault(c => c.Title == "Азия")
+                    }
+                };
+                сontinents?.AddRange(cont);
+                countries?.AddRange(count);
+                SaveChanges();
+            }
         }
     }
 }
--- Dz26.02.2024/Dz26.02.2024/Program.cs
+++ Dz26.02.2024/Dz26.02.2024/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Options;
 namespace Dz26._02._2024 {
     public static class Program {
         public static void PrintAllInfo(DbContextOptions<Context> options, Context context) {
-            var allInfo = context.Сountries.Include(c => c.Continent).ToList();
+            var allInfo = context.countries.Include(c => c.Continent).ToList();
             foreach (var country in allInfo) {
                 Console.WriteLine($"ID: {country.Id}");
                 Console.WriteLine($"Название: {country.Title}");
@@ -20,50 +20,57 @@ namespace Dz26._02._2024 {
             }
         }
         public static void PrintTitles(DbContextOptions<Context> options, Context context) {
-            var titles = context.Сountries.Select(c => c.Title).ToList();
+            var titles = context.countries.Select(c => c.Title).ToList();
             Console.WriteLine("Названия стран:");
             foreach (var title in titles) Console.WriteLine(title);
+            Console.WriteLine();
         }
         public static void PrintCapitals(DbContextOptions<Context> options, Context context) {
-            var capitals = context.Сountries.Select(c => c.Capital).ToList();
+            var capitals = context.countries.Select(c => c.Capital).ToList();
             Console.WriteLine("Столицы стран:");
             foreach (var capital in capitals) Console.WriteLine(capital);
+            Console.WriteLine();
         }
         public static void PrintEuropean(DbContextOptions<Context> options, Context context) {
-            var european = context.Сountries.Where(c => c.Continent != null 
+            var european = context.countries.Where(c => c.Continent != null 
             && c.Continent.Title == "Европа").ToList();
             Console.WriteLine("Названия европейских стран:");
             foreach (var country in european) Console.WriteLine(country.Title);
+            Console.WriteLine();
         }
         public static void PrintExtraCountries(DbContextOptions<Context> options, Context context) {
-            var extraCountries = context.Сountries.Where(c => c.Area > 1000000).ToList();
+            var extraCountries = context.countries.Where(c => c.Area > 1000000).ToList();
             Console.WriteLine("Страны с площадью больше 1000000 км^2:");
             foreach (var country in extraCountries) Console.WriteLine($"Название: {country.Title}, Площадь: {country.Area}");
+            Console.WriteLine();
         }
         public static void PrintAECountries(DbContextOptions<Context> options, Context context) {
-            var aeCountries = context.Сountries.Where(c => c.Title != null && c.Title.Contains("а") 
+            var aeCountries = context.countries.Where(c => c.Title != null && c.Title.Contains("а") 
             && c.Title.Contains("е")).ToList();
             Console.WriteLine("Страны с буквами 'а' и 'е' в названии:");
             foreach (var country in aeCountries) Console.WriteLine(country.Title);
+            Console.WriteLine();
         }
         public static void PrintStartA(DbContextOptions<Context> options, Context context) {
-            var countriesA = context.Сountries.Where(c => c.Title != null 
+            var countriesA = context.countries.AsEnumerable().Where(c => c.Title != null 
             && c.Title.StartsWith("А", StringComparison.OrdinalIgnoreCase)).ToList();
             Console.WriteLine("Страны, название которых начинается с буквы 'А':");
             foreach (var country in countriesA) Console.WriteLine(country.Title);
+            Console.WriteLine();
         }
         public static void PrintExtraArea(DbContextOptions<Context> options, Context context) {
-            var extraArea = context.Сountries.Where(c => c.Area >= 500000 && c.Area <= 2000000).ToList();
+            var extraArea = context.countries.Where(c => c.Area >= 500000 && c.Area <= 2000000).ToList();
             Console.WriteLine("Страны с площадью от 500000 до 2000000:");
             foreach (var country in extraArea) Console.WriteLine($"Название: {country.Title}, Площадь: {country.Area}");
+            Console.WriteLine();
         }
         public static void PrintExtraPopulation(DbContextOptions<Context> options, Context context) {
-            var extraPopulation = context.Сountries.Where(c => c.Population > 1100000).ToList();
+            var extraPopulation = context.countries.Where(c => c.Population > 1100000).ToList();
             Console.WriteLine("Страны с населением больше 1100000 человек:");
             foreach (var country in extraPopulation) Console.WriteLine($"Название: {country.Title}, Население: {country.Population}");
         }
         public static void Main(string[] args) {
-            try { 
+            try {
                 var builder = new ConfigurationBuilder();
                 builder.SetBasePath(Directory.GetCurrentDirectory());
                 builder.AddJsonFile("settings.json");
